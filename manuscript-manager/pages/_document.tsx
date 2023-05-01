60eadfaa72324437bd4422bea635c1f883cda57c
Shayne Geilman <shaynegeilman@gmail.com>
2023-05-01 16:11:53 +0800

    Chakra UI installed and setup

--- manuscript-manager/pages/_document.tsx
+++ manuscript-manager/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { Providers } from "../providers/providers";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <Providers>
+          <Main />
+          <NextScript />
+        </Providers>
       </body>
     </Html>
-  )
+  );
 }
