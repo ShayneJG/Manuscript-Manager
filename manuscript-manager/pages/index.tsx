7df299e22b553fc292dae82e569c2307e511fdf3
Shayne Geilman <shaynegeilman@gmail.com>
2023-05-01 22:08:39 +0800

    Update index.tsx

--- manuscript-manager/pages/index.tsx
+++ manuscript-manager/pages/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Summary from "../components/stats/summary";
 const inter = Inter({ subsets: ["latin"] });
@@ -9,6 +8,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <h1>TEST PAGE</h1>
+      <div id="stat-test"></div>
       <Summary label="month" statistic={1200} lastStatistic={1000} />
     </main>
   );
