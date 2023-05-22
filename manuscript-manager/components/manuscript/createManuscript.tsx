a9b735327ba07fe1deb27fb8be6c120d482d33be
Shayne Geilman <shaynegeilman@gmail.com>
2023-05-22 12:26:01 +0800

    shifted order of form, added wordcount

--- manuscript-manager/components/manuscript/createManuscript.tsx
+++ manuscript-manager/components/manuscript/createManuscript.tsx
@@ -16,13 +16,25 @@ import { ChangeEvent, useState } from "react";
 
 export default function CreateManuscript() {
   const [manuscriptID, setManuscriptID] = useState<string>("");
-  const [manuscriptDate, setManuscriptDate] = useState<Date>(new Date());
+  const [date, setDate] = useState<Date>(new Date());
+  const [wordcount, setWordcount] = useState<number>();
   return (
     <div>
-      <FormControl isRequired>
+      <FormControl id="date">
+        <FormLabel>Date</FormLabel>
+        <Box>
+          {/* Chakra UI does not have a date picker component. It has an input of type date,
+          but that was too weird to use, so we are using react-datepicker here, which simplifies things a lot */}
+          <DatePicker
+            dateFormat={"dd/MM/yyyy"}
+            selected={date}
+            onChange={(date: Date) => setDate(date)}
+          />
+        </Box>
+      </FormControl>
+      <FormControl id="manuscript ID" isRequired>
         <FormLabel>Manuscript ID</FormLabel>
         <Input
-          id="Manuscript ID"
           placeholder="Manuscript ID"
           value={manuscriptID}
           onChange={(e: ChangeEvent<HTMLInputElement>) =>
@@ -30,17 +42,16 @@ export default function CreateManuscript() {
           }
         />
       </FormControl>
-      <FormControl>
-        <FormLabel>Date</FormLabel>
-        <Box>
-          {/* Chakra UI does not have a date picker component. It has an input of type date,
-          but that was too weird to use, so we are using react-datepicker here, which simplifies things a lot */}
-          <DatePicker
-            dateFormat={"dd/MM/yyyy"}
-            selected={manuscriptDate}
-            onChange={(date: Date) => setManuscriptDate(date)}
-          />
-        </Box>
+
+      <FormControl id="wordcount" isRequired>
+        <FormLabel>Wordcount</FormLabel>
+        <Input
+          type="number"
+          value={wordcount}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setWordcount(e.target.valueAsNumber);
+          }}
+        />
       </FormControl>
     </div>
   );
