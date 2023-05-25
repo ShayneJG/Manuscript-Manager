4887d4f6b3363e6d684b41c30174387d88b2b22f
Shayne Geilman <shaynegeilman@gmail.com>
2023-05-25 12:05:01 +0800

    userProvider wrapper added to app

--- manuscript-manager/pages/_app.tsx
+++ manuscript-manager/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
     </ChakraProvider>
   );
 }
