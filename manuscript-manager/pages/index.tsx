80042ed25e74b42bf2615757dbd77c5c8d673161
AthenasCode <emoore5203@gmail.com>
2023-05-25 13:05:12 +0800

    added placeholders and retitled page

--- manuscript-manager/pages/index.tsx
+++ manuscript-manager/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <h1>TEST PAGE</h1>
+      <h1>MANUSCRIPT MANAGER</h1>
       <div id="stat-test">
         <CreateManuscript />
         <ManuscriptTable data={fakeManuscripts} caption="Test Table" />
