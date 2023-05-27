19f9e2a8cfbfdc8bea9c2f3303a3369c9989054e
Shayne Geilman <shaynegeilman@gmail.com>
2023-05-27 11:02:35 +0800

    Update profileDropdownIcon.tsx

---
+++ manuscript-manager/components/profile/profileDropdownIcon.tsx
@@ -0,0 +1,7 @@
+/*
+This component renders the profile icon and its subsequent dropdown menu
+*/
+
+import { UseUser } from "@auth0/nextjs-auth0/dist/client/use-user";
+
+export default function ProfileAvatarDropdown() {}
