012c6727124592ce8a825c81f30bdbae513cff10
Shayne Geilman <shaynegeilman@gmail.com>
2023-05-27 23:50:45 +0800

    reworked avatar/menu again

--- manuscript-manager/components/profile/profileDropdownIcon.tsx
+++ manuscript-manager/components/profile/profileDropdownIcon.tsx
@@ -1,9 +1,21 @@
 /*
 This component renders the profile icon and its subsequent dropdown menu
 */
-import { Spinner, Alert, AlertIcon, Avatar, Button } from "@chakra-ui/react";
+import {
+  Spinner,
+  Alert,
+  AlertIcon,
+  Avatar,
+  Button,
+  Flex,
+  Menu,
+  MenuGroup,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+} from "@chakra-ui/react";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import ProfileMenu from "./menu";
 
 export default function ProfileAvatarDropdown() {
   const { user, error, isLoading } = useUser();
@@ -22,13 +34,49 @@ export default function ProfileAvatarDropdown() {
 
   const name: string = user?.name || "Guest";
   const image: string | undefined = user?.picture || undefined;
+
+  if (user === undefined) {
+    return (
+      <Flex>
+        <Avatar name={name} src={image} />
+        <h1>{name}</h1>
+        <Button as="a" href="/api/auth/login" colorScheme="blue">
+          Log In
+        </Button>
+      </Flex>
+    );
+  }
+
   return (
     //User exists: Show profile and menu
-    <div>
-      <Avatar name={name} src={image} />
+    <Flex>
+      <Menu>
+        <MenuButton>
+          <Avatar name={name} src={image} />
+        </MenuButton>
+        <MenuList>
+          <MenuGroup title="Profile">
+            <MenuItem>My Account</MenuItem>
+            <MenuItem>Manuscript Search</MenuItem>
+            <MenuItem>Monthly Data</MenuItem>
+            {user ? (
+              <MenuItem as="a" href="/api/auth/logout">
+                Log Out
+              </MenuItem>
+            ) : (
+              <MenuItem as="a" href="/api/auth/login">
+                Log In
+              </MenuItem>
+            )}
+          </MenuGroup>
+          <MenuDivider />
+          <MenuGroup title="Help">
+            <MenuItem>FAQ</MenuItem>
+            <MenuItem>Contact</MenuItem>
+          </MenuGroup>
+        </MenuList>
+      </Menu>
       <h1>{name}</h1>
-      <ProfileMenu />
-      {!user && <Button>LogIn</Button>}
-    </div>
+    </Flex>
   );
 }
