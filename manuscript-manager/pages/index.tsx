fd9f24171691e91e74b05731967b77510d647dbc
Shayne Geilman <shaynegeilman@gmail.com>
2023-05-27 23:22:02 +0800

    redesigned test page and added new components

--- manuscript-manager/pages/index.tsx
+++ manuscript-manager/pages/index.tsx
@@ -6,22 +6,38 @@ import feb from "@/data/Feb.json";
 import ManuscriptTable from "@/components/table/table";
 import { fakeManuscripts } from "@/data/Manuscripts";
 import CreateManuscript from "@/components/manuscript/createManuscript";
+import { Grid, GridItem } from "@chakra-ui/react";
+import ProfileAvatarDropdown from "@/components/profile/profileDropdownIcon";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    <Grid
+      templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+      gridTemplateRows={"50px 1fr 30px"}
+      gridTemplateColumns={"1fr"}
     >
-      <h1>MANUSCRIPT MANAGER</h1>
-      <div id="stat-test">
-        <a href="/api/auth/login">Login</a>
-        <a href="/api/auth/logout">Logout</a>
+      <GridItem area="header">
+        <h1>MANUSCRIPT MANAGER</h1>
+      </GridItem>
+      <GridItem area="nav">
+        <ProfileAvatarDropdown />
         <CreateManuscript />
-        <ManuscriptTable data={fakeManuscripts} caption="Test Table" />
+      </GridItem>
+      <GridItem area="main">
+        {" "}
+        <main
+          className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+        >
+          <ManuscriptTable data={fakeManuscripts} caption="Test Table" />
+        </main>{" "}
+      </GridItem>
+      <GridItem area="footer">
         <AtAGlance month={jan} prevMonth={feb} />
-      </div>
-    </main>
+      </GridItem>
+    </Grid>
   );
 }
