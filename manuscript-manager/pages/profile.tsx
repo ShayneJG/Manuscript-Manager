6a18a7271f3518f0549e010c7fe3a077e3f95f02
Shayne Geilman <shaynegeilman@gmail.com>
2023-05-29 11:51:58 +0800

    beginning to setup profile

--- manuscript-manager/pages/profile.tsx
+++ manuscript-manager/pages/profile.tsx
@@ -1,7 +1,33 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { Alert, AlertIcon } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertIcon,
+  Center,
+  Box,
+  FormLabel,
+  FormControl,
+  Input,
+  Flex,
+  FormHelperText,
+  Button,
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 export default function Profile() {
+  //useUser is async
   const { user, error, isLoading } = useUser();
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [rate, setRate] = useState<number>();
+  //this handles the async nature of useUser. When the data finally comes in,
+  //it updates the required fields
+  useEffect(() => {
+    if (user?.name) {
+      setName(user.name);
+    }
+    if (user?.email) {
+      setEmail(user.email);
+    }
+  }, [user]);
 
   if (isLoading) return <div>Loading...</div>;
   if (error)
@@ -12,14 +38,47 @@ export default function Profile() {
       </Alert>
     );
 
-  const picture: string | undefined = user?.picture || "imageMissing";
-  const name: string = user?.name || "nameMissing";
+  const onSubmit = () => {};
   return user ? (
-    <div>
-      <img src={picture} alt={name} />
-      <h2>{name}</h2>
-      <p>{user.email}</p>
-    </div>
+    <Center flexDirection="column">
+      <h1>Profile</h1>
+      <Box>
+        <form>
+          <FormControl isRequired>
+            <FormLabel>Name</FormLabel>
+            <Input
+              type="text"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            ></Input>
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Email</FormLabel>
+            <Input
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            ></Input>
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Pay Rate</FormLabel>
+            <Input
+              type="number"
+              value={rate}
+              onChange={(e) => {
+                setRate(e.target.valueAsNumber);
+              }}
+            ></Input>
+            <FormHelperText>e.g., 0.0070</FormHelperText>
+          </FormControl>
+          <Button type="submit">Update</Button>
+        </form>
+      </Box>
+    </Center>
   ) : (
     // handles when user is undefined.
     <div>
