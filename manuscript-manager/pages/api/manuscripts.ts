cbe5df8b2f3d20ddea3fc7684f4498be2d4981c9
AthenasCode <emoore5203@gmail.com>
2023-05-31 16:57:53 +0800

    added getServerSideProps to retrieve database info

--- manuscript-manager/pages/api/manuscripts.ts
+++ manuscript-manager/pages/api/manuscripts.ts
@@ -1,18 +1,19 @@
+// This is an API endpoint for accessing the manuscripts from the database
+
 // The lib/mongodb file contains instructions on how to connect to MongoDB Atlas cluster
 // Within that file the instance of our connection is cached, so subsequent requests do not have to reconnect
 import clientPromise from "../../lib/mongodb";
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-// API route handler that gets executed when the .../api/properties route is called
+// API route handler that gets executed when the .../api/manuscript route is called
 export default async (req: NextApiRequest, res: NextApiResponse) => {
    try {
        const client = await clientPromise; // clientPromise is a function that gets the instance of the MongoDB database
        const db = client.db("test"); // specifies which collection in the database we are accessing
 
        const manuscripts = await db // uses MongoDB MQL queries to access info from the database
-           .collection("manuscripts")
+           .collection("manuscripts") // specifies the subsection/subcollection
            .find({})
-           .limit(5)
            .toArray();
 
        res.json(manuscripts); // serves the response in JSON format to the browser - this will be displayed when visiting the /api/manuscripts path in browser
@@ -20,3 +21,4 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
        console.error(e);
    }
 };
+
--- manuscript-manager/pages/index.tsx
+++ manuscript-manager/pages/index.tsx
@@ -1,15 +1,21 @@
 import { Inter } from "next/font/google";
 import AtAGlance from "@/components/stats/atAGlance";
-
 import jan from "@/data/Jan.json";
 import feb from "@/data/Feb.json";
 import ManuscriptTable from "@/components/table/table";
 import { fakeManuscripts } from "@/data/Manuscripts";
 import CreateManuscript from "@/components/manuscript/createManuscript";
+import { GetServerSideProps } from "next"; // in-built getServerSideProps type
+import clientPromise from "../lib/mongodb";
+import { ManuscriptDBType } from "@/types/dbManuscriptsTest";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+
+export default function Home({ manuscripts }: ManuscriptDBType[]) {
+  
+  console.log(manuscripts);
+  
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -24,4 +30,21 @@ export default function Home() {
       </div>
     </main>
   );
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const client = await clientPromise; 
+  const db = client.db("test"); 
+  
+  const data = await db
+           .collection("manuscripts") 
+           .find({})
+           .toArray();
+           
+  const manuscripts = JSON.parse(JSON.stringify(data));
+  
+  return {
+    props: { manuscripts: manuscripts},
+  }
 }
