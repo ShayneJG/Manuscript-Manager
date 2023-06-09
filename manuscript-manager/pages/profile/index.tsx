da991c19e774c04c1abd2f7647a02104fd4c576b
Shayne Geilman <shaynegeilman@gmail.com>
2023-06-09 11:23:50 +0800

    created profile page and user util

---
+++ manuscript-manager/pages/profile/index.tsx
@@ -0,0 +1,22 @@
+import {
+    Spinner,
+    Alert,
+    AlertIcon,
+    Avatar,
+    Button,
+    Flex,
+    Menu,
+    MenuGroup,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+  } from "@chakra-ui/react";
+  import { useSession } from "next-auth/react"
+
+  function Profile() {
+
+    
+
+
+  }
