ed4ee02b719b5f8643fb14a395a5ba4b88fba2fa
Shayne Geilman <shaynegeilman@gmail.com>
2023-06-09 15:58:15 +0800

    Create getUser.ts

---
+++ manuscript-manager/pages/api/user/getUser.ts
@@ -0,0 +1,33 @@
+import clientPromise from "@/lib/mongodb";
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
+
+
+
+export default  async function getUser(req: NextApiRequest, res: NextApiResponse) {
+    const { email } = req.query; // Assuming the email is sent in the request body
+     
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) {
+      res.send({ error: 'Session not authenticated' });
+    } else {
+        const client = await clientPromise; // clientPromise is a function that gets the instance of the MongoDB database
+        const db = client.db("test")
+
+      // Find the document with the same email
+      const user = await db.collection('users').findOne({ email });
+      
+      if (user) {
+        res.send({ user });
+      } else {
+        res.send({ error: 'User not found' });
+      } 
+    }
+  } catch (error) {
+    res.status(500).send({ error: 'Internal server error' });
+  }
+}
+
