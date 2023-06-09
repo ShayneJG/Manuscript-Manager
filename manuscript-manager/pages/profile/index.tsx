c1f3774928f9baedce49ec6e2ba424bfaba371ac
Shayne Geilman <shaynegeilman@gmail.com>
2023-06-09 12:09:03 +0800

    profile page created

--- manuscript-manager/pages/profile/index.tsx
+++ manuscript-manager/pages/profile/index.tsx
@@ -1,3 +1,4 @@
+import getUser from "@/utils/getUser";
 import {
     Spinner,
     Alert,
@@ -10,13 +11,63 @@ import {
     MenuButton,
     MenuList,
     MenuItem,
-    MenuDivider,
+    MenuDivider, FormControl,
+    Center,
+    Box,
+    FormLabel,
+    Input,FormHelperText
   } from "@chakra-ui/react";
   import { useSession } from "next-auth/react"
+import { useState } from "react";
 
-  function Profile() {
 
+   export default function Profile() {
+    const user = getUser();
+
+    const {status} = useSession();
+
+    //state
+    const [payRate, setPayRate] = useState<number>(user.payRate)
     
-
-
+    if(status === "unauthenticated") {
+        return <Alert>
+            <AlertIcon>User not Logged in</AlertIcon>
+        </Alert>
+    } else if(status === "loading") {
+        return <Spinner></Spinner>
+    } else return (<Center flexDirection="column">
+    <h1>Profile</h1>
+    <Box>
+      <form>
+        <FormControl isDisabled>
+          <FormLabel>Name</FormLabel>
+          <Input
+            type="text"
+            value={user.name}
+            
+          ></Input>
+        </FormControl>
+        <FormControl isDisabled>
+          <FormLabel>Email</FormLabel>
+          <Input
+            type="email"
+            value={user.email}
+            
+          ></Input>
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Pay Rate</FormLabel>
+          <Input
+            type="number"
+            value={payRate}
+            onChange={(e) => {
+              setPayRate(e.target.valueAsNumber);
+            }}
+          ></Input>
+          <FormHelperText>e.g., 0.0070</FormHelperText>
+        </FormControl>
+        <Button type="submit">Update</Button>
+      </form>
+    </Box>
+  </Center>)
   }
