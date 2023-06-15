8be8dae289ca881c6bd63c051f2ec5729b9ff64d
AthenasCode <emoore5203@gmail.com>
2023-06-15 21:32:52 +0800

    further refactoring
    pulled manuscript handling from createManuscript into its own file to reduce size of component

--- manuscript-manager/components/manuscript/createManuscript.tsx
+++ manuscript-manager/components/manuscript/createManuscript.tsx
@@ -20,8 +20,8 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { ChangeEvent, useState } from "react";
-import { DM_Serif_Display } from "next/font/google";
 import { useEffect } from "react";
+import { handleManuscripts } from "@/utils/handleManuscripts";
 //this component handles the creation of new manuscripts, and will eventually send the manuscript as a request to the backend.
 
 interface CreateManuscriptProps {
@@ -35,7 +35,7 @@ export default function CreateManuscript({
 }: CreateManuscriptProps) {
   const [manuscriptID, setManuscriptID] = useState<string>("");
   const [date, setDate] = useState<Date>(new Date());
-  const [wordCount, setWordCount] = useState<number>();
+  const [wordCount, setWordCount] = useState<number | undefined>();
   const [latex, setLatex] = useState<boolean>(false);
   const [double, setDouble] = useState<boolean>(false);
   const [triple, setTriple] = useState<boolean>(false);
@@ -84,25 +84,13 @@ export default function CreateManuscript({
     }
   }
 
-  // Submits manuscript to DB or updates a given manuscript (m), and then sets today's manuscripts in state by getting them from DB
-  async function submitOrUpdateManuscript(
-    action: string,
-    reset: () => void,
-    get: () => void,
-    m?: ManuscriptType
-  ) {
-    // add manuscript to db
-    // capture date manuscript was submitted (previously Mongoose did this for us)
-
-    const createdAt =
-      action === "POST" ? new Date().toISOString() : m?.createdAt;
-    const updatedAt = new Date().toISOString();
-    const apiPath =
-      action === "POST"
-        ? "/api/manuscripts/postManuscript"
-        : `/api/manuscripts/updateManuscript?id=${m?._id}`;
-
-    const manuscript = {
+  // Handles submission of a manuscript
+  async function handleSubmit(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    handleManuscripts(
+      "POST",
+      resetManuscriptState,
+      getTodaysManuscripts,
       date,
       manuscriptID,
       wordCount,
@@ -112,52 +100,6 @@ export default function CreateManuscript({
       bonus,
       turnAround,
       authorBio,
-      createdAt,
-      updatedAt,
-    };
-
-    try {
-      const requestOptions = {
-        method: action,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(manuscript),
-      };
-      let response;
-      if (action === "PATCH" && m) {
-        response = await fetch(apiPath, requestOptions);
-      } else {
-        response = await fetch(apiPath, requestOptions);
-      }
-
-      const json = await response.json();
-
-      if (!response.ok) {
-        action === "POST"
-          ? console.log("There was an error submitting the manuscript.")
-          : console.log("There was an error updating the manuscript.");
-      }
-
-      if (response.ok) {
-        // TODO: reset state values and input fields
-        console.log("Response ok:", json);
-        reset();
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-    // update state by fetching new manuscripts for today
-    get();
-  }
-
-  // Handles submission of a manuscript
-  async function handleSubmit(e: MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-    submitOrUpdateManuscript(
-      "POST",
-      resetManuscriptState,
-      getTodaysManuscripts,
       undefined
     );
   }
@@ -165,10 +107,19 @@ export default function CreateManuscript({
   // Handles updating of a manuscript
   async function handleUpdate(e: MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
-    submitOrUpdateManuscript(
+    handleManuscripts(
       "PATCH",
       resetManuscriptState,
       getTodaysManuscripts,
+      date,
+      manuscriptID,
+      wordCount,
+      latex,
+      double,
+      triple,
+      bonus,
+      turnAround,
+      authorBio,
       manuscriptToUpdate
     );
   }
