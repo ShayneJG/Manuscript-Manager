3ed11d09e272c650ec0a5c88a6b021a3f7ca8561
Shayne Geilman <shaynegeilman@gmail.com>
2023-06-16 14:18:14 +0800

    added user data to getserversideprops on main

--- manuscript-manager/pages/index.tsx
+++ manuscript-manager/pages/index.tsx
@@ -5,18 +5,21 @@ import feb from "@/data/Feb.json";
 import ManuscriptTable from "@/components/table/table";
 import { fakeManuscripts } from "@/data/Manuscripts";
 import CreateManuscript from "@/components/manuscript/createManuscript";
+import { getServerSession } from "next-auth";
 
 import { GetServerSideProps } from "next"; // in-built getServerSideProps type
 import clientPromise from "../lib/mongodb";
 import { ManuscriptType } from "@/types/manuscripts";
 import ProfileAvatarDropdown from "@/components/profile/profileIcon";
-
+import {authOptions} from "@/pages/api/auth/[...nextauth]"; 
+import UserType from "@/types/user";
 const inter = Inter({ subsets: ["latin"] });
 
 
-export default function Home({ manuscripts }: { manuscripts: ManuscriptType[] }) {
+export default function Home({ manuscripts, user}: { manuscripts: ManuscriptType[], user: UserType}) {
   
-  console.log(manuscripts);
+  // console.log(manuscripts);
+  console.log("Client side user is: ", user)
   
   return (
     <main
@@ -25,7 +28,7 @@ export default function Home({ manuscripts }: { manuscripts: ManuscriptType[] })
       <h1>TEST PAGE</h1>
       <ProfileAvatarDropdown />
       <div id="stat-test">
-        <CreateManuscript />
+        <CreateManuscript user={user} />
         <ManuscriptTable data={manuscripts} caption="Test data from MongoDB" />
         <AtAGlance month={jan} prevMonth={feb} />
       </div>
@@ -51,7 +54,34 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   // So we have to add this workaround of stringifying the data we get back, and then reparsing it:
   const manuscripts = JSON.parse(JSON.stringify(data));
   
+
+  // gets userdata from the session and passes a user object to the page. This is basically the same data as the user session, but it has the payrate added. 
+
+  const session = await getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  )
+
+  let user = {}
+
+    if(session) {
+      // Find the document with the same email
+      const email = session?.user?.email
+      const userData = await db.collection('users').findOne({email})
+      
+      //creates and passes user object for email and payrate usage. Probably not the correct way, as this overlaps with getSession, but it works. 
+        user = {name: userData?.name, email: userData?.email, payRate: userData?.payRate ? userData.payRate : null}
+        console.log("user updated: ", user)
+      }
+
+
+      
+  
+  console.log("server-side user is: ", user)
+
   return {
-    props: { manuscripts: manuscripts },
+    props: { manuscripts: manuscripts,
+            user: user },
   }
 }
