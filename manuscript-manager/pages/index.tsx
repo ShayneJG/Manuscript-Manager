a47df40e387a70d228984decc5cc639059e2fbc6
Shayne Geilman <shaynegeilman@gmail.com>
2023-06-22 20:11:03 +0800

    removed redundant state

--- manuscript-manager/pages/index.tsx
+++ manuscript-manager/pages/index.tsx
@@ -73,7 +73,7 @@ export default function Home(props: HomeProps) {
               <ManuscriptTable
                 data={manuscriptsInState}
                 setManuscriptToUpdate={setManuscriptToUpdate}
-                manuscriptsInState={manuscriptsInState}
+                
                 setManuscriptsInState={setManuscriptsInState}
                 caption="(Today's manuscripts)"
               />
