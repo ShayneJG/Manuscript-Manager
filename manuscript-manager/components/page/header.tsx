61ab9bf50b7339d76923a82b6c4d6eac6716b9f8
Shayne Geilman <shaynegeilman@gmail.com>
2023-06-26 13:32:37 +0800

    created header component

---
+++ manuscript-manager/components/page/header.tsx
@@ -0,0 +1,13 @@
+import { Heading } from "@chakra-ui/react";
+import ProfileAvatarDropdown from "../profile/profileIcon";
+
+
+
+//default header component for all pages
+//assumes parent div/main has styling for page positioning
+export default function Header () {
+    return <header className="flex justify-between pb-8">
+        <Heading size="lg">Manuscript Manager</Heading>
+        <ProfileAvatarDropdown />
+    </header>
+}
