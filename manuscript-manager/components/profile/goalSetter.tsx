cdf45a091d3e9b65cb4201c2aa68b5366f7378c2
Shayne Geilman <shaynegeilman@gmail.com>
2023-06-26 17:56:53 +0800

    Create goalSetter.tsx

---
+++ manuscript-manager/components/profile/goalSetter.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+
+
+
+
+
+export default function GoalSetter() {
+
+    const [day, setDay] = useState<number>(5)
+    const [week, setWeek] = useState<number>(400)
+    const [month, setMonth] = useState<number>(1600)
+
+    
+
+
+    return (<div>
+
+    </div>)
+
+
+}
