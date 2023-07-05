0cc9dbbdb050221a0d90de0985051e9792de3f3b
Shayne Geilman <shaynegeilman@gmail.com>
2023-07-05 14:42:14 +0800

    added link to title => home

--- manuscript-manager/components/page/header.tsx
+++ manuscript-manager/components/page/header.tsx
@@ -7,7 +7,7 @@ import ProfileAvatarDropdown from "../profile/profileIcon";
 //assumes parent div/main has styling for page positioning
 export default function Header () {
     return <header className="flex justify-between pb-8">
-        <Heading size="lg">Manuscript Manager</Heading>
+        <Heading size="lg"><a href="/">Manuscript Manager</a></Heading>
         <ProfileAvatarDropdown />
     </header>
 }
