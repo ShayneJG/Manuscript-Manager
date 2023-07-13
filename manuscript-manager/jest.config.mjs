5f37ec446714c2c784aa0932ee52eb37b49a1f85
Shayne Geilman <shaynegeilman@gmail.com>
2023-07-13 17:04:11 +0800

    added moduleNameMapper for utils

--- manuscript-manager/jest.config.mjs
+++ manuscript-manager/jest.config.mjs
@@ -8,10 +8,14 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
+  //this is required by jest to understand imports properly
+  moduleNameMapper: {
+    '^@/utils/(.*)$': '<rootDir>/utils/$1',
+  },
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
  
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jest-environment-jsdom'
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
