4dc165b3c8abe18948aec1618761898424d01de7
Shayne Geilman <shaynegeilman@gmail.com>
2023-07-31 16:01:35 +0800

    header css, added subtitle

--- manuscript-manager/components/page/header.tsx
+++ manuscript-manager/components/page/header.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import ProfileAvatarDropdown from "../profile/profileIcon";
 import Link from "next/link";
 
@@ -6,10 +6,13 @@ import Link from "next/link";
 //assumes parent div/main has styling for page positioning
 export default function Header() {
   return (
-    <header className="flex justify-between pb-8">
-      <Heading size="lg">
-        <Link href="/">Manuscript Manager</Link>
-      </Heading>
+    <header className="flex justify-between pb-8 text-font italic border-b-2 mb-5">
+      <Box>
+        <Heading size="2xl">
+          <Link href="/">Manuscript Manager</Link>
+        </Heading>
+        <Heading size="sm">Manuscripts made easy</Heading>
+      </Box>
       <ProfileAvatarDropdown />
     </header>
   );
