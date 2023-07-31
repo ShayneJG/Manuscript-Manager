5bba37dcc677eb0b3c9433453e240ceb3d50ef1f
Shayne Geilman <shaynegeilman@gmail.com>
2023-07-31 17:52:27 +0800

    removed unneeded imports

---
+++ manuscript-manager/pages/search.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/page/header";
+import Head from "next/head";
+
+
+
+
+export default function Search() {
+
+
+
+    return(
+        <main>
+            <Head>
+                <title>Manuscript Search</title>
+            </Head>
+        </main>
+    )
+}
