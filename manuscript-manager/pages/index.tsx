a307c2006c4203c25d1fdc9df3f8c0e4c8f35377
Shayne Geilman <shaynegeilman@gmail.com>
2023-07-31 16:01:51 +0800

    changed background colour

--- manuscript-manager/pages/index.tsx
+++ manuscript-manager/pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home(props: HomeProps) {
   console.log("Client side user is: ", user);
 
   return (
-    <main className={`min-h-screen py-16 px-24 ${inter.className}`}>
+    <main className={`min-h-screen py-16 px-24 bg-background ${inter.className}`}>
       <Header />
       <Grid templateColumns="repeat(10, 1fr)" gap="12">
         <GridItem colSpan={3} id="sidebar">
