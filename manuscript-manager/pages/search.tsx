da80d5a6df5c38417d057ae1b43e5b17c7a40517
Shayne Geilman <shaynegeilman@gmail.com>
2023-08-07 15:34:34 +0800

    added enter key functionality for search

--- manuscript-manager/pages/search.tsx
+++ manuscript-manager/pages/search.tsx
@@ -15,7 +15,7 @@ import {
   ModalOverlay,
   Spinner,
   useDisclosure,
-  useToast
+  useToast,
 } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
@@ -29,7 +29,7 @@ interface SearchProps {
   user: UserType;
 }
 
-export default function Search({user}: SearchProps) {
+export default function Search({ user }: SearchProps) {
   const { status, data: session } = useSession();
   const [manIdError, setManIdError] = useState(false);
   const [manuscriptID, setManuscriptID] = useState("");
@@ -40,42 +40,44 @@ export default function Search({user}: SearchProps) {
     ManuscriptType[]
   >([]);
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  useEffect(() => {manuscriptToUpdate ? onOpen() : onClose()}, [manuscriptToUpdate]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  useEffect(() => {
+    manuscriptToUpdate ? onOpen() : onClose();
+  }, [manuscriptToUpdate]);
 
   const toast = useToast();
 
   const handleSubmit = async () => {
-
     setManIdError(false);
     if (manuscriptID === "") {
       setManIdError(true);
       return undefined;
     } else {
       try {
-        const result = await fetch(`api/manuscripts/getManuscript?identification=${manuscriptID}`);
-        
-        if(result.status === 404) {
+        const result = await fetch(
+          `api/manuscripts/getManuscript?identification=${manuscriptID}`
+        );
+
+        if (result.status === 404) {
           toast({
             title: "Manuscript not found",
             description: `${manuscriptID} cannot be found, please try again`,
             status: "info",
             duration: 2000,
             isClosable: true,
-          })
+          });
           return undefined;
         }
 
-        if(result.ok) {
+        if (result.ok) {
           const manuscript = await result.json();
-          
+
           setManuscriptsInState([manuscript]);
         }
-      }
-      catch(e: any) {
+      } catch (e: any) {
         console.error(e.message);
       }
-      }
+    }
   };
 
   if (status === "unauthenticated") {
@@ -94,68 +96,86 @@ export default function Search({user}: SearchProps) {
           <title>Manuscript Search</title>
         </Head>
 
-         <Modal isOpen={isOpen} onClose={onClose}>
-         <ModalOverlay />
-         <ModalContent>
-          <CreateManuscript
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <CreateManuscript
               user={user}
               setManuscriptToUpdate={setManuscriptToUpdate}
               manuscriptToUpdate={manuscriptToUpdate}
               setManuscriptsInState={setManuscriptsInState}
-            /></ModalContent></Modal>
+            />
+          </ModalContent>
+        </Modal>
 
         <FormControl isInvalid={manIdError} isRequired>
           <FormLabel>Manuscript ID</FormLabel>
-          <Input value={manuscriptID} onChange={(e) => {setManuscriptID(e.target.value)}}></Input>
+          <Input
+            value={manuscriptID}
+            onChange={(e) => {
+              setManuscriptID(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                handleSubmit();
+              }
+            }}
+          ></Input>
           <FormErrorMessage>
             Please insert a valid manuscript ID
           </FormErrorMessage>
-          <Button type='submit' onClick={() => {handleSubmit()}}>Search</Button>
+          <Button
+            type="submit"
+            onClick={() => {
+              handleSubmit();
+            }}
+          >
+            Search
+          </Button>
         </FormControl>
-        
+
         <ManuscriptTable
-                includeDate
-                data={manuscriptsInState}
-                setManuscriptToUpdate={setManuscriptToUpdate}
-                setManuscriptsInState={setManuscriptsInState}
-                
-              />
+          includeDate
+          data={manuscriptsInState}
+          setManuscriptToUpdate={setManuscriptToUpdate}
+          setManuscriptsInState={setManuscriptsInState}
+        />
       </main>
     );
   }
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const client = await clientPromise;
+  const db = client.db();
 
-const client = await clientPromise;
-const db = client.db();
+  // gets userdata from the session and passes a user object to the page. This is basically the same data as the user session, but it has the payrate added.
 
-// gets userdata from the session and passes a user object to the page. This is basically the same data as the user session, but it has the payrate added.
+  const session = await getServerSession(context.req, context.res, authOptions);
 
-const session = await getServerSession(context.req, context.res, authOptions);
-
-let user: UserType = {
-  name: "guest",
-  email: "guest@email.com",
-  payRate: Number(process.env.PAYRATE_DEFAULT),
-};
-
-if (session) {
-  // Find the document with the same email
-  const email = session?.user?.email;
-  const userData = await db.collection("users").findOne({ email });
-
-  //creates and passes user object for email and payrate usage. Probably not the correct way, as this overlaps with getSession, but it works.
-  user = {
-    name: userData?.name,
-    email: userData?.email,
-    payRate: userData?.payRate ? userData.payRate : null,
+  let user: UserType = {
+    name: "guest",
+    email: "guest@email.com",
+    payRate: Number(process.env.PAYRATE_DEFAULT),
   };
-  // console.log("user updated: ", user);
-}
-return {
-  props: {
-    user,
-  },
+
+  if (session) {
+    // Find the document with the same email
+    const email = session?.user?.email;
+    const userData = await db.collection("users").findOne({ email });
+
+    //creates and passes user object for email and payrate usage. Probably not the correct way, as this overlaps with getSession, but it works.
+    user = {
+      name: userData?.name,
+      email: userData?.email,
+      payRate: userData?.payRate ? userData.payRate : null,
+    };
+    // console.log("user updated: ", user);
+  }
+  return {
+    props: {
+      user,
+    },
+  };
 };
-}
